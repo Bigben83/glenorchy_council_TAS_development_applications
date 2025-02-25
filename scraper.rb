3730cef0f63c1249ee8e9ef3b34888513e558322
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-25 15:48:07 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,38 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'nokogiri'
+require 'open-uri'
+require 'date'
+require 'logger'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find something on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Set up a logger to log the scraped data
+logger = Logger.new(STDOUT)
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# URL of the Glenorchy City Council planning applications page
+main_page_url = "https://www.gcc.tas.gov.au/services/planning-and-building/planning-and-development/planning-applications/"
+
+# Open and parse the main page
+main_page = Nokogiri::HTML(open(main_page_url))
+
+# Loop through each content block in the main listing
+main_page.css('.content-block').each do |content_block|
+  # Extract the address
+  address = content_block.at_css('.content-block__title a').text.strip
+
+  # Extract the closing date and parse it to the desired format
+  closing_date = content_block.at_css('.content-block__date').text.strip
+  closing_date = Date.strptime(closing_date.gsub("Closes:", "").strip, "%d %B %Y").to_s
+
+  # Extract the description (if present)
+  description = content_block.at_css('.content-block__description p')
+  description = description ? description.text.strip : "No description available"
+
+  # Extract the PDF download link
+  pdf_link = content_block.at_css('.content-block__button a')['href']
+
+  # Log the extracted data
+  logger.info("Address: #{address}, Closing Date: #{closing_date}, Description: #{description}, PDF Link: #{pdf_link}")
+  
+  # If you need to handle additional details, such as geolocation, it can be extracted as follows:
+  lat = content_block.at_css('.content-block__map-link')['data-lat']
+  lng = content_block.at_css('.content-block__map-link')['data-lng']
+  logger.info("Latitude: #{lat}, Longitude: #{lng}")
+end
